a95d387e3e2884bab6daf34e50d0b189310d8c41
zm05130513 <zm05130513@163.com>
2020-12-17 23:23:35 +0800

    LoginActivity.java

--- app/src/main/java/com/example/day01_2/persenter/MainPersenterImpI.java
+++ app/src/main/java/com/example/day01_2/persenter/MainPersenterImpI.java
@@ -2,14 +2,39 @@ package com.example.day01_2.persenter;
 
 import com.example.day01_2.base.BasePersenter;
 import com.example.day01_2.contract.MainContract;
+import com.example.day01_2.model.MainModelImp;
+import com.example.day01_2.model.UserBean;
 import com.example.day01_2.utils.net.INetCallBack;
 
-public class MainPersenterImpI extends BasePersenter
-implements MainContract.IMainModel {
-    private MainContract.IMainModel mainModel;
-    private MainContract.IMainView iMainView;
-    @Override
-    public <T> void getLonginData(String url, INetCallBack<T> callBack) {
+public class MainPersenterImpI extends BasePersenter<MainContract.IMainView> implements MainContract.ImainPersenter {
+
+    private MainContract.IMainModel iMainModel;
+
+    public MainPersenterImpI(MainContract.IMainView mainView) {
+        this.iMainModel = new MainModelImp(this);
 
     }
+    @Override
+    public void login(String name,String password) {
+
+//        URL就在P层确定,不要从Activity中传递过来
+        iMainModel.getLonginData("", new INetCallBack<UserBean>() {
+            @Override
+            public void onSuccess(UserBean userBean) {
+                iview.getData(userBean);
+            }
+
+            @Override
+            public void onFail(String err) {
+
+            }
+        });
+    }
+
+    @Override
+    public void loginResult(String result) {
+
+    } 
+
+
 }
--- app/src/main/java/com/example/day01_2/view/LoginActivity.java
+++ app/src/main/java/com/example/day01_2/view/LoginActivity.java
@@ -1,16 +1,87 @@
 package com.example.day01_2.view;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.day01_2.R;
+import com.example.day01_2.base.BaseActivity;
+import com.example.day01_2.contract.MainContract;
+import com.example.day01_2.model.UserBean;
+import com.example.day01_2.persenter.MainPersenterImpI;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseActivity<MainPersenterImpI>
+implements MainContract.IMainView {
+    private EditText editTextTextPersonName;
+    private EditText editTextTextPassword;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    protected void initData() {
+        persenter.login(getUserName(), getPassword());
+    }
+
+    @Override
+    protected void initView() {
+        editTextTextPersonName=findViewById(R.id.editTextTextPersonName);
+        editTextTextPassword=findViewById(R.id.editTextTextPassword);
+    }
+
+    @Override
+    protected int getLayotID() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public MainPersenterImpI add() {
+        return new MainPersenterImpI(this);
+    }
+
+    /*
+    登录按钮
+    * */
+    public void loginButton(View view) {
+        if (!TextUtils.isEmpty(getUserName()) && !TextUtils.isEmpty(getPassword())) {
+            persenter.login(getUserName(), getPassword());
+        }
+    }
+
+    @Override
+    public MainPersenterImpI getPersenter() {
+        return new MainPersenterImpI(this);
+    }
+
+    @Override
+    public void getLoginData(String string) {
+
+    }
+
+    public void loginButtin(View view) {
+        if (!TextUtils.isEmpty(getUserName()) && !TextUtils.isEmpty(getPassword())) {
+            persenter.login(getUserName(), getPassword());
+        }
+    }
+
+    @Override
+    public String getUserName() {
+        return editTextTextPersonName.getText().toString();
+    }
+
+    @Override
+    public String getPassword() {
+        return editTextTextPassword.getText().toString();
+    }
+
+    @Override
+    public void getData(UserBean string) {
+
+    }
+
 }
