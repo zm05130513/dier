d322ed78ca87f47ba7eabdbc65a20f21a73cc6ee
zm05130513 <zm05130513@163.com>
2020-12-17 20:24:36 +0800

    xml文件

---
+++ app/src/main/java/com/example/day01_2/view/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.day01_2.view;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.day01_2.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
