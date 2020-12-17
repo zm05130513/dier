5c9112346e6b5c3a198cb89e2bbb2b8fc9e3e1bc
zm05130513 <zm05130513@163.com>
2020-12-17 23:03:41 +0800

    MainModelImp.java

--- app/src/main/java/com/example/day01_2/model/MainModelImp.java
+++ app/src/main/java/com/example/day01_2/model/MainModelImp.java
@@ -2,11 +2,18 @@ package com.example.day01_2.model;
 
 import com.example.day01_2.contract.MainContract;
 import com.example.day01_2.utils.net.INetCallBack;
+import com.example.day01_2.utils.net.RetrofitUtils;
 
 public class MainModelImp implements MainContract.IMainModel {
+    private MainContract.ImainPersenter persenter;
+
+    public MainModelImp(MainContract.ImainPersenter persenter) {
+        this.persenter = persenter;
+    }
 
     @Override
     public <T> void getLonginData(String url, INetCallBack<T> callBack) {
-
+persenter.loginResult("登陆成功了");
+        RetrofitUtils.getInstance().get(url,callBack);
     }
 }
