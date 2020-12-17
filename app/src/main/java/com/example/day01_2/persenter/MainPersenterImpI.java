67db3312b6a05c650196e9ab66d18c4f53aeb05e
zm05130513 <zm05130513@163.com>
2020-12-17 23:05:53 +0800

    MainPersenterlImp.java

--- app/src/main/java/com/example/day01_2/persenter/MainPersenterImpI.java
+++ app/src/main/java/com/example/day01_2/persenter/MainPersenterImpI.java
@@ -1,4 +1,15 @@
 package com.example.day01_2.persenter;
 
-public class MainPersenterImpI extends {
+import com.example.day01_2.base.BasePersenter;
+import com.example.day01_2.contract.MainContract;
+import com.example.day01_2.utils.net.INetCallBack;
+
+public class MainPersenterImpI extends BasePersenter
+implements MainContract.IMainModel {
+    private MainContract.IMainModel mainModel;
+    private MainContract.IMainView iMainView;
+    @Override
+    public <T> void getLonginData(String url, INetCallBack<T> callBack) {
+
+    }
 }
