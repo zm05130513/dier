5c022124072b03643ecd9dcefa06a4c143946287
zm05130513 <zm05130513@163.com>
2020-12-17 22:28:07 +0800

    INetWorkInterface.java

--- app/src/main/java/com/example/day01_2/utils/net/INetWorkInterface.java
+++ app/src/main/java/com/example/day01_2/utils/net/INetWorkInterface.java
@@ -1,5 +1,9 @@
 package com.example.day01_2.utils.net;
 
+import java.util.HashMap;
+
 public interface INetWorkInterface {
-    //public<T> void get(String url,)
+    public<T> void get(String url,INetCallBack<T> callBack);
+    public<T> void post(String url,INetCallBack<T> callBack);
+    public<T> void post(String url, HashMap<String,String> map,INetCallBack<T> callBack);
 }
